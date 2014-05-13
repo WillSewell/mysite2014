b2b2ab39d74c450a5979f514a05dc9823438038d
Will Sewell <me@willsewell.name>
2014-05-13 13:57:10 +0100

    Added a custom deploy script using rsync. The site is now live!

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import           Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -65,3 +65,9 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+config :: Configuration
+config = defaultConfiguration
+    { deployCommand = "rsync --checksum -ave ssh \
+                      \_site/* me@willsewell.name:mysite2014"
+    }
