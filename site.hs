3a91ca036cbeee4699e61a4a187198bad6f877bb
Will Sewell <me@willsewell.com>
2016-04-16 15:41:14 +0100

    Use 2 char indent and remove column alignment

--- site.hs
+++ site.hs
@@ -1,90 +1,90 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import Data.Monoid (mappend)
+import Hakyll
 
-
---------------------------------------------------------------------------------
 main :: IO ()
-main = hakyllWith config $ do
+main =
+  hakyllWith config $ do
     match ("images/*" .||. "bower_components/**") $ do
-        route   idRoute
-        compile copyFileCompiler
+      route idRoute
+      compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route idRoute
+      compile compressCssCompiler
 
     match "contact.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
 
     create ["blog.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let blogCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Blog"                `mappend`
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let
+          blogCtx =
+            listField "posts" postCtx (return posts) `mappend`
+            constField "title" "Blog" `mappend`
+            defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/blog.html" blogCtx
-                >>= loadAndApplyTemplate "templates/default.html" blogCtx
-                >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/blog.html" blogCtx
+          >>= loadAndApplyTemplate "templates/default.html" blogCtx
+          >>= relativizeUrls
 
     match "index.md" $ do
-        route   $ setExtension "html"
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+      route $ setExtension "html"
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let
+          indexCtx =
+            listField "posts" postCtx (return posts) `mappend`
+            constField "title" "Home" `mappend`
+            defaultContext
 
-            pandocCompiler
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+        pandocCompiler
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
     create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend` bodyField "description"
-            posts <- fmap (take 10) . recentFirst =<<
-                loadAllSnapshots "posts/*" "content"
-            renderAtom myFeedConfiguration feedCtx posts
+      route idRoute
+      compile $ do
+        let feedCtx = postCtx `mappend` bodyField "description"
+        posts <-
+          fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+        renderAtom myFeedConfiguration feedCtx posts
 
-
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext
 
 config :: Configuration
-config = defaultConfiguration
-    { deployCommand = "rsync --checksum -ave ssh \
-                      \_site/* me@willsewell.name:mysite2014"
+config =
+  defaultConfiguration
+    { deployCommand =
+      "rsync --checksum -ave ssh _site/* me@willsewell.name:mysite2014"
     }
 
 myFeedConfiguration :: FeedConfiguration
-myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "Will Sewell"
+myFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "Will Sewell"
     , feedDescription = "The blog of Will Sewell."
-    , feedAuthorName  = "Will Sewell"
+    , feedAuthorName = "Will Sewell"
     , feedAuthorEmail = "me@willsewell.com"
-    , feedRoot        = "http://willsewell.com"
+    , feedRoot = "http://willsewell.com"
     }
